f2622ff4e04eb9c36bd9a1197ed602f70f484e95
NguyenVanHoang19 <nguyenvanhoang34iuh@gmail.com>
2020-11-18 23:05:57 +0700

    update location maps

---
+++ app/src/main/java/com/nguyenvanhoang/foodapp/view/user/Constaints.java
@@ -0,0 +1,10 @@
+package com.nguyenvanhoang.foodapp.view.user;
+
+public class Constaints {
+    public static final String PACKGAE_NAME = "com.nguyenvanhoang.foodapp.view.user";
+    public static final String RESULT_DATA_KEY = PACKGAE_NAME + ".RESULT_DATA_KEY";
+    public static final String RECEIVER = PACKGAE_NAME + ".RECEIVER";
+    public static final String LOCATION_DATA_EXTRA = PACKGAE_NAME + ".LOCATION_DATA_EXTRA";
+    public static final int SUCCESS_RESULT =1 ;
+    public static final int FAILURE_RESULT = 0;
+}
--- app/src/main/java/com/nguyenvanhoang/foodapp/view/user/UserActivity.java
+++ app/src/main/java/com/nguyenvanhoang/foodapp/view/user/UserActivity.java
@@ -1,36 +1,65 @@
 package com.nguyenvanhoang.foodapp.view.user;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.ResultReceiver;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.nguyenvanhoang.foodapp.R;
 import com.nguyenvanhoang.foodapp.view.cart.AddCartActivity;
 
 public class UserActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_LOCATION_PERMISSION = 1;
     private Toolbar toolbar ;
     private AppBarLayout appBarLayout;
     private CollapsingToolbarLayout collapsingToolbarLayout;
     private Button btnDangNhap,btnDangKy,btnDangXuat,btnGioHang;
+    private TextView tvViTriHienTai;
+    private ResultReceiver resultReceiver;
     public static boolean TRANG_THAI_DANG_NHAP = false;
     public static String Email_Login = "false";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        resultReceiver = new AddressResultReceiver(new Handler());
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
         btnDangKy = (Button) findViewById(R.id.btnDangKy);
         btnDangNhap = (Button) findViewById(R.id.btnDangNhap);
         btnDangXuat = (Button) findViewById(R.id.btnDangXuat);
         btnGioHang = (Button) findViewById(R.id.btnGioHang);
+        tvViTriHienTai = (TextView) findViewById(R.id.tvViTriHienTai);
+        // check quyen
+        if (ContextCompat.checkSelfPermission(
+                getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(UserActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_LOCATION_PERMISSION);
+        } else {
+            getCurrentLocation();
+        }
         btnDangXuat.setVisibility(View.GONE);
         initActionBar();
         if(TRANG_THAI_DANG_NHAP){
@@ -94,6 +123,71 @@ public class UserActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+            } else {
+                Toast.makeText(this, "Chua Cho Phep Quyen Vi Tri", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+    @SuppressLint("MissingPermission")
+    public void getCurrentLocation() {
+        final LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationServices.getFusedLocationProviderClient(UserActivity.this)
+                .requestLocationUpdates(locationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        super.onLocationResult(locationResult);
+                        LocationServices.getFusedLocationProviderClient(UserActivity.this)
+                                .removeLocationUpdates(this);
+                        if(locationResult != null && locationResult.getLocations().size() > 0){
+                            int latestLocationIndex = locationResult.getLocations().size() - 1;
+                            double latitude =
+                                    locationResult.getLocations().get(latestLocationIndex).getLatitude();
+                            double longtitude =
+                                    locationResult.getLocations().get(latestLocationIndex).getLongitude();
+                            Location location = new Location("providerNA");
+                            location.setLatitude(latitude);
+                            location.setLongitude(longtitude);
+                            fetchAddressFromLatLong(location);
+                        }
+                        else{
+                        }
+
+                    }
+                }, Looper.getMainLooper());
+    }
+    private void fetchAddressFromLatLong(Location location){
+        Intent intent = new Intent(this, ServiceAddress.class);
+        intent.putExtra(Constaints.RECEIVER,resultReceiver);
+        intent.putExtra(Constaints.LOCATION_DATA_EXTRA,location);
+        startService(intent);
+    }
+    private class AddressResultReceiver extends ResultReceiver {
+        AddressResultReceiver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+            super.onReceiveResult(resultCode, resultData);
+            if(resultCode ==  Constaints.SUCCESS_RESULT){
+                tvViTriHienTai.setText(resultData.getString(Constaints.RESULT_DATA_KEY));
+            }
+            else{
+                Toast.makeText(UserActivity.this,resultData.getString(Constaints.RESULT_DATA_KEY),Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
